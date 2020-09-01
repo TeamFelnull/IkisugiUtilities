adce5371b40def4fdece7586f03afde404561d19
MORIMORI <morimori.0317@outlook.jp>
2020-09-02 02:16:55 +0900

    frist

---
+++ src/main/java/red/felnull/ikisugiutilities/IkisugiUtilities.java
@@ -0,0 +1,15 @@
+package red.felnull.ikisugiutilities;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(IkisugiUtilities.MODID)
+public class IkisugiUtilities {
+    public static final String MODID = "ikisugiutilities";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public IkisugiUtilities() {
+    }
+
+}
